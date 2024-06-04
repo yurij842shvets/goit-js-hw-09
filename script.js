2b55f00fad8786a4543a0bcba943025b0ed6eafd
yurij842shvets <yurij2011shvets@gmail.com>
2024-06-04 23:18:42 +0300

    add js and html files

---
+++ script.js
@@ -0,0 +1,41 @@
+// const button = document.querySelector('#btn');
+// console.log(button);
+// const input = document.querySelector('#input');
+// console.log(input);
+
+// function getInputValue() {
+//     const newText = input.value;
+//     button.innerHTML = newText;
+//     input.value = ''
+// }
+
+// button.addEventListener('click', getInputValue)
+
+
+
+// const title = document.querySelector('h2')
+// const img = document.querySelector('.kahoot-img')
+// const button2 = document.querySelector('.button2')
+// console.log(button2);
+
+// button2.addEventListener('click', function() {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Black_Color.jpg/1536px-Black_Color.jpg'
+// })
+
+
+
+// const button3 = document.querySelector('.button3')
+// const link = document.querySelector('a')
+// const img2 = document.querySelector('.blue-img')
+
+// function changeURLAddress() {
+//     link.href = 'https://uk.wikipedia.org/wiki/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0_%D1%81%D1%82%D0%BE%D1%80%D1%96%D0%BD%D0%BA%D0%B0'
+// }
+// button3.addEventListener('click', changeURLAddress)
+
+
+
+const listItem = document.querySelector('li');  
+console.log(listItem);
+
+listItem.firstChild.textContent = 'How are you?'
